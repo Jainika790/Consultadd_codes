af834fe04a0d95242ed5b3e57c242891c0869929
Jainika123 <jainikadaga30082001@gmail.com>
2022-10-19 19:31:58 +0530

    Updated after removing pointer from merge sort

--- 18_TUE_DAY7.py/D7_Q2.py
+++ 18_TUE_DAY7.py/D7_Q2.py
@@ -1,49 +1,49 @@
-def merge(arr, left, middle, right):
+#MERGE_SORT
+# Here l is left, r is right, and m is middle element.
+def merge(arr, l, m, r):
 
-    n1 = middle - left + 1
-    n2 = right - middle
+    n1 = m - l + 1
+    n2 = r - m
     left_arr = [0]*n1
     right_arr = [0]*n2
 
     for i in range(n1):
-        left_arr[i] = arr[left+i]
+        left_arr[i] = arr[l+i]
 
     for i in range(n2):
-        right_arr[i] = arr[middle+1+i]
+        right_arr[i] = arr[m+1+i]
 
-    ptrLeft = 0
-    ptrRight = 0
-    k = left
+    L = 0
+    R = 0
+    k = l
 
-    while (ptrLeft < n1 and ptrRight < n2):
-        if (left_arr[ptrLeft] <= right_arr[ptrRight]):
-            arr[k] = left_arr[ptrLeft]
-            ptrLeft += 1
+    while (L < n1 and R < n2):
+        if (left_arr[L] <= right_arr[R]):
+            arr[k] = left_arr[L]
+            L += 1
         else:
-            arr[k] = right_arr[ptrRight]
-            ptrRight += 1
+            arr[k] = right_arr[R]
+            R += 1
         k += 1
 
-    # Copy the remaining elements of leftHalves if there are any.
-    while (ptrLeft < n1):
-        arr[k] = left_arr[ptrLeft]
-        ptrLeft += 1
+    while (L < n1):
+        arr[k] = left_arr[L]
+        L += 1
         k += 1
 
-    # Copy the remaining elements of rightHalves if there are any.
-    while (ptrRight < n2):
-        arr[k] = right_arr[ptrRight]
-        ptrRight += 1
+    while (R < n2):
+        arr[k] = right_arr[R]
+        R += 1
         k += 1
 
-def mergeSortNew(arr, left, right):
-    if(left < right):
-        middle = (left+right)//2
-        mergeSortNew(arr, left, middle)
-        mergeSortNew(arr, middle+1, right)
+def mergeSortNew(arr, l, r):
+    if(l < r):
+        m = (l+r)//2
+        mergeSortNew(arr, l, m)
+        mergeSortNew(arr, m+1, r)
 
         # returns merged sorted list.
-        merge(arr, left, middle, right)
+        merge(arr, l, m, r)
 
 def mergeSort(arr, n):
     mergeSortNew(arr, 0, n-1)
